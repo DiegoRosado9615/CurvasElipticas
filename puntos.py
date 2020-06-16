122a2445c21a977a9e563ff3635ae7ae29278be4
jonatan <jonatanaacm@ciencias.unam.mx>
2020-06-15 19:26:29 -0500

    empezando suma extendida

--- puntos.py
+++ puntos.py
@@ -25,7 +25,7 @@ class Punto:
         y2=punto2.y
         if(self.esIgual(punto2)):
             x1=x1*x1
-            return (int(((3*x1) +a )/ (2*y1) ))%p
+            return (int(((3*x1) +a )/(2*y1) ))%p
         else:
             if(x2==x1):
                 return "Division entre 0"
@@ -48,6 +48,14 @@ class Punto:
             punto3.x=(lambd * lambd)-x1-x2
             punto3.y=lambd*(x1-punto3.x)-y1
             return punto3
+    def sumaExtendida(self, punto):
+    	##la idea es sumar el punto tantas veces hasta que ya no sea posible.
+    	##es decir sacar la suma de forma lineal 
+    	"""
+    		ejemplo 
+    		3P = P + P + P en vez de 2P + P
+
+    	"""
 
     def __str__(self):
         """Imprimir la cadena"""
@@ -73,16 +81,26 @@ class Curva:
 					print("\t("+str(i)+","+str(j)+"),")
 		print("\tO.")
 	""" Metodo que verifica si un punto esta dentro de la curva"""
-	##def esta(self, x, y):
-
-
+	def esta(self, punto, curva):
+		 y = (punto.y ** 2)
+		 resto = (punto.x ** 3) + (curva.a * punto.x) + (curva.b)
+		 if y == (resto % curva.p) :
+		 	return True
+		 else : 
+		 	return False
+	def __str__ (self):
+		if (self.a==1):
+			return "y² = x³ + x + "+str(self.b)+ " con Z: "+str(self.p) 
+		else :
+			return "y² = x³ + "+str(self.a)+" x + "+str(self.b)+ " con Z: "+str(self.p) 
 
 
 punto1= Punto(2,7)
 punto2= Punto (331,-6000)
-punto3= Punto()
 curva = Curva(1,6,11)
 curva.calculaPuntosEncurva()
 print("punto 1: "+ str(punto1))
 print("punto 2: "+ str(punto2))
 print("suma: "+str(punto1.suma(punto2,20,35)))##Esto es q2, y estoy sacando q3
+print("esta: "+ str(punto1)+ " \n en "+str(curva) )
+print(curva.esta(punto1,curva ))
