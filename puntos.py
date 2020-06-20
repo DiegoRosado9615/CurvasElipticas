d6d159d5c75f13cb0aa82fbcd823277cd30751cb
LoboStark <rocadie9615@ciencias.unam.mx>
2020-06-20 12:04:12 -0500

    Pregunta uno terminada

--- puntos.py
+++ puntos.py
@@ -1,78 +1,81 @@
 """Clase que modela puntos"""
 class Punto:
-    """Constructor de Puntos"""
-    def __init__(self,num1,num2):
-        self.x=num1
-        self.y=num2
-    """Metodo que obtiene el inverso multiplicativo """
-    def inversoMultiplicativo(self,a, n):
-	    a%=n;
-	    for i in range(1,n):
-	        if ((a * i) % n == 1) :
-	            return i;
-	    return 1
-    """Metodo que nos dice si 2 puntos son iguales o no
+	"""Constructor de Puntos"""
+	def __init__(self,num1,num2):
+		self.x=num1
+		self.y=num2
+	"""Metodo que obtiene el inverso multiplicativo """
+	def inversoMultiplicativo(self,a, n):
+		a%=n;
+		for i in range(1,n):
+			if ((a * i) % n == 1) :
+				return i;
+		return 1
+	"""Metodo que nos dice si 2 puntos son iguales o no
 	devuelve true si son iguales y false en otro caso """
-    def esIgual(self,punto2):
-        x1=self.x
-        y1=self.y
-        x2=punto2.x
-        y2=punto2.y
-        if(x1==x2 and y1==y2):
-            return True
-        else:
-            return False
-    """Metodo que obtiene el valor de la lambda"""
-    def lambd(self,punto2,a,p):
-    	x1=self.x
-    	y1=self.y
-    	x2=punto2.x
-    	y2=punto2.y
-    	if(self.esIgual(punto2)):
-    		x1=x1*x1
-    		return ((((3*x1) + a)*self.inversoMultiplicativo(2*y1,p))) % p
-    	else:
-    		return ((y2-y1)*self.inversoMultiplicativo(x2-x1, p))%p
-    """Metodo que permite sumar 2 puntos,recibe el punto con el que se va a
+	def esIgual(self,punto2):
+		x1=self.x
+		y1=self.y
+		x2=punto2.x
+		y2=punto2.y
+		if(x1==x2 and y1==y2):
+			return True
+		else:
+			return False
+	"""Metodo que obtiene el valor de la lambda"""
+	def lambd(self,punto2,a,p):
+		x1=self.x
+		y1=self.y
+		x2=punto2.x
+		y2=punto2.y
+		
+
+		if(self.esIgual(punto2)):
+			x1=x1*x1
+			print(self.inversoMultiplicativo(2*y1,p))
+			return ((((3*x1) + a)*self.inversoMultiplicativo(2*y1,p))) % p
+		else:
+			return ((y2-y1)*self.inversoMultiplicativo(x2-x1, p))%p
+	"""Metodo que permite sumar 2 puntos,recibe el punto con el que se va a
 	sumar  y devuelve un nuevo punto o un mensaje de infinito"""
-    def suma(self,punto2,a,p):
-        x1=self.x
-        y1=self.y
-        x2=punto2.x
-        y2=punto2.y
-        if( x1==x2 and ((y1*-1)== y2 )):
-            punto3=Punto(-1,-1)
-            return punto3
-        else:
-            punto3=Punto(0,0)
-            lambd=punto1.lambd(punto2,a,p)
-            punto3.x=((lambd * lambd)-x1-x2)%p
-            punto3.y=((lambd*(x1-punto3.x))-y1)%p
-        return punto3
-    """Metodo que genera la suma de puntos de manera extendida"""
-    def sumaExtendida(self,punto,a,p):
-    	listaPuntos = []
-    	contador = 0
-    	puntoNuevo = punto
-    	listaPuntos.append(punto)
-    	while contador < p:
-    		puntoNuevo=punto.suma(puntoNuevo,a,p)
-    		listaPuntos.append(puntoNuevo)
-    		contador = contador + 1
-    	return listaPuntos
+	def suma(self,punto2,a,p):
+		x1=self.x
+		y1=self.y
+		x2=punto2.x
+		y2=punto2.y
+		if( x1==x2 and ((y1*-1)== y2 )):
+			punto3=Punto(-1,-1)
+			return punto3
+		else:
+			punto3=Punto(0,0)
+			lambd=punto1.lambd(punto2,a,p)
+			punto3.x=((lambd * lambd)-x1-x2)%p
+			punto3.y=((lambd*(x1-punto3.x))-y1)%p
+			return punto3
+	"""Metodo que genera la suma de puntos de manera extendida"""
+	def sumaExtendida(self,punto,a,p):
+		listaPuntos = []
+		contador = 0
+		puntoNuevo = punto
+		listaPuntos.append(punto)
+		while contador < p:
+			puntoNuevo=punto.suma(puntoNuevo,a,p)
+			listaPuntos.append(puntoNuevo)
+			contador = contador + 1
+		return listaPuntos
 
-    """Metodo que imprime un punto"""
-    def __str__(self):
-        return "(" + str( self.x ) + " , " + str(self.y)+")"
+	"""Metodo que imprime un punto"""
+	def __str__(self):
+		return "(" + str( self.x ) + " , " + str(self.y)+")"
 
-    """Metodo que saca el mcd"""
-    def mcd(self,num1,num2):
-	    resta=0
-	    while (num2>0):
-	       	resta=num2
-	       	num2=num1%num2
-	       	num1=resta
-	    return num1
+	"""Metodo que saca el mcd"""
+	def mcd(self,num1,num2):
+		resta=0
+		while (num2>0):
+			resta=num2
+			num2=num1%num2
+			num1=resta
+		return num1
 
 """ Clase curva: modela una curva eliptica """
 class Curva:
@@ -110,6 +113,7 @@ class Curva:
 
 #Haciendo tarea ejercicio 4
 punto1= Punto(15,-4)
+print(punto1.lambd(punto1,-20,7))
 punto2= Punto(0,14)
 punto3= Punto(17,21)
 
@@ -118,7 +122,8 @@ punto3= Punto(17,21)
 #print("punto 1: \n\t"+ str(punto1))
 #print()
 #print("suma extendida")
-lista = punto1.suma(punto1,-20,35)
+print("Estoy jalando")
+lista = punto1.suma(punto1,-20,7) 
 
 print(lista)
 #for i in range(0,len(lista)):
